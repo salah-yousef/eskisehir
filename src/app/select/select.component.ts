d43379e1cefdc0213cd07da552160ddb4b566756
Salah Ismail <salah_ismail@Salahs-MacBook-Pro.local>
2019-08-27 18:33:44 +0300

    select component

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-select',
-  templateUrl: './select.component.html',
-  styleUrls: ['./select.component.scss']
+  templateUrl: './select.component.html'
 })
 export class SelectComponent implements OnInit {
-
-  constructor() { }
+  amount: number;
+  adet: number;
+  CCSelectForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
+  
+  public ccvMask = {
+    mask: [/\d/, /\d/, /\d/],
+    guide: false,
+    keepCharPositions: false,
+    placeholderChar: '_',
+    showMask: false,
+    modelClean: true
+  };
 
   ngOnInit() {
+    this.CCSelectForm = this.fb.group({     
+      adet: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(3)
+      ]),
+      
+    });
+     //this.CCSelectForm.valueChanges.subscribe(console.log);
+    document.body.classList.add('pbesk');
+  }
+  
+  onKey($event){
+    //console.log(this.adet);   
+    console.log(this.CCSelectForm.value.adet);
+    this.amount = this.CCSelectForm.value.adet * 26;
+     
   }
 
 }
