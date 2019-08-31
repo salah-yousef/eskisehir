ed43bc6854bbe13337a4564d3a82d91c8c6fddd4
salah-ismail <salah.yousef@pharmatolia.com>
2019-08-31 05:05:43 +0300

    Download image from canvas element.

--- src/app/donator-form/donator-form.component.ts
+++ src/app/donator-form/donator-form.component.ts
@@ -10,12 +10,9 @@ export class DonatorFormComponent implements OnInit, AfterViewInit {
   donationForm: FormGroup;
   step = { bar: 50, step1: 'check', step2: 'active', step3: 'error' };
   decodedData: {};
-  vertical = 380;
-  horizontal = 1110;
-  textWidthArea = 826;
-  textWidth = 0;
-  extraSpace = 0;
-  leftOffset = 0;
+  myImage;
+  topOffset = 380;
+  leftOffset = 1355;
   sources = {
     silver: 'assets/images/layout/badge-silver.jpg',
     bronze: 'assets/images/layout/badge-bronz.jpg',
@@ -23,9 +20,10 @@ export class DonatorFormComponent implements OnInit, AfterViewInit {
   };
   width = 1920;
   height = 1280;
-
+  userName = '';
   public context: CanvasRenderingContext2D;
   @ViewChild('myCanvas') myCanvas;
+  link = document.createElement('a');
 
   constructor(
     public ss: SharedService,
@@ -33,57 +31,83 @@ export class DonatorFormComponent implements OnInit, AfterViewInit {
   ) { }
 
   ngAfterViewInit(): void {
-    this.drawCanvas("");
+    this.drawCanvas('');
   }
-  
+
   ngOnInit() {
     this.decodedData = this.ss.decodedData;
     this.donationForm = this.formBuilder.group({
       name: new FormControl('', [
         Validators.required,
         Validators.minLength(3),
-        Validators.maxLength(20)
+        Validators.maxLength(16)
       ])
     });
     document.body.classList.add('pbesk');
   }
 
   donationSubmit(form) {
-    console.log(form.value.name);
+    this.userName = form.value.name;
     this.drawCanvas(form.value.name);
+    setTimeout(() => {
+      this.link.click();
+      document.body.removeChild(this.link);
+    }, 1000);
   }
 
   drawCanvas(name: string): void {
-    console.log(name.offsetWidth);
     const canvasEl: HTMLCanvasElement = this.myCanvas.nativeElement;
-    canvasEl.style.letterSpacing = '2px';
-    this.context = canvasEl.getContext("2d");
-    let image = new Image();
+    canvasEl.style.letterSpacing = '1px';
+    this.context = canvasEl.getContext('2d');
+    const image = new Image();
     canvasEl.width = this.width;
     canvasEl.height = this.height;
     image.onload = () => {
-      this.context.drawImage(image, 0, 0, canvasEl.width, canvasEl.height );
-      this.context.fillStyle = "#fff";
-      this.context.font = "67px Gotham";
-      this.textWidth = name.length * 67;
-      this.extraSpace = (this.textWidthArea - this.textWidth) / 2;
-      console.log(this.extraSpace);
-      this.leftOffset = this.extraSpace + this.horizontal;
-      console.log(this.leftOffset );
-      this.context.fillText(name.toUpperCase(), this.leftOffset, this.vertical);
-    }
+      this.context.drawImage(image, 0, 0, canvasEl.width, canvasEl.height);
+      this.context.fillStyle = '#fff';
+      this.context.font = 'normal normal 900 50px Roboto Mono';
+      this.context.textAlign = 'center';
 
-    let numOfCertificates = this.ss.decodedData.BaseAmount / 26;
+      this.context.fillText(name.toUpperCase(), this.leftOffset, this.topOffset);
+      this.myImage = atob(canvasEl.toDataURL('image/jpg').replace('data:image/png;base64,', ''));
+      this.createImage();
+    };
+
+    const numOfCertificates = this.ss.decodedData.BaseAmount / 26;
     if (numOfCertificates >= 1 && numOfCertificates <= 9) {
-      console.log("Silver");
+      console.log('Silver');
       image.src = this.sources.silver;
-    }
-    else if (numOfCertificates >= 10 && numOfCertificates <= 49) {
-      console.log("Bronze");
+    } else if (numOfCertificates >= 10 && numOfCertificates <= 49) {
+      console.log('Bronze');
       image.src = this.sources.bronze;
     } else {
-      console.log("Gold");
+      console.log('Gold');
       image.src = this.sources.gold;
     }
   }
+
+  createImage() {
+    const byteNumbers = new Array(this.myImage.length);
+    for (let i = 0; i < this.myImage.length; i++) {
+      byteNumbers[i] = this.myImage.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+
+    const blob = new Blob([byteArray], { 'type': 'image/jpeg' });
+
+    if (navigator.msSaveBlob) {
+      const filename = this.userName;
+      navigator.msSaveBlob(blob, filename);
+    } else {
+
+      this.link.href = URL.createObjectURL(blob);
+
+      this.link.setAttribute('visibility', 'hidden');
+      this.link.download = this.userName;
+      document.body.appendChild(this.link);
+
+    }
+  }
+
 }
